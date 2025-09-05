a3edf64ad521f95440d5fcfc5081689c47f88dd9
Ismael Valenzuela <ismaelv@protonmail.com>
2025-09-04 17:31:43 -0700

    The game's display will now retain it's state whenever the game is started and paused. generate placement will generate a new display.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import {
+  createContext,
   createElement,
   memo,
+  useContext,
   useEffect,
+  useMemo,
   useReducer,
   useRef,
   useState,
@@ -19,46 +22,155 @@ interface Controlls {
 
 interface Display {
   displayRef: React.RefObject<HTMLElement | null>;
+  run: boolean;
 }
 
 const DISPLAY_WIDTH: number = 30,
   DISPLAY_HEIGHT: number = 15;
 
-const Display = memo(({ displayRef }: Display) => {
-  const generateDisplay = () => {
-    const display = [];
-    for (let i = 0; i < DISPLAY_HEIGHT; i++) {
-      const rowcolumns = [];
-      for (let j = 0; j < DISPLAY_WIDTH; j++) {
-        rowcolumns.push(
-          createElement("div", {
-            className: "life-container",
-            "data-x": j,
-            "data-y": i,
-            "data-living": Math.floor(Math.random() * 2) === 1,
-            onClick: (ev) => {
-              console.log(
-                `x: ${ev.currentTarget.dataset.x}, y: ${ev.currentTarget.dataset.y}`
-              );
-            },
-          })
-        );
-      }
-      const row = createElement(
-        "div",
-        {
-          className: "life-row",
-        },
-        rowcolumns
-      );
-      display.push(row);
-    }
-    return display;
+const GameContext = createContext<null | {
+  run: boolean;
+}>(null);
+
+function useGameContext() {
+  const gameContext = useContext(GameContext);
+  if (!gameContext) {
+    throw new Error(
+      "gameContext has to be used within <GameContext.Provider>!"
+    );
+  }
+  return gameContext;
+}
+
+const isAlive = (x: number, y: number) => {
+  return document.querySelector(
+    `div[data-x="${x}"][data-y="${y}"][data-living="true"]`
+  )
+    ? true
+    : false;
+};
+
+const countLivingNeighbors = (x: number, y: number) => {
+  let livingNeighbors = 0;
+  const incrementLivingNeighbors = (x: number, y: number) => {
+    if (isAlive(x, y)) livingNeighbors++;
   };
+
+  const topY = y - 1;
+  const botY = y + 1;
+
+  const leftX = x - 1;
+  const rightX = x + 1;
+
+  // top row
+  incrementLivingNeighbors(leftX, topY);
+  incrementLivingNeighbors(x, topY);
+  incrementLivingNeighbors(rightX, topY);
+  // middle row
+  incrementLivingNeighbors(leftX, y);
+  incrementLivingNeighbors(rightX, y);
+  // bottom row
+  incrementLivingNeighbors(leftX, botY);
+  incrementLivingNeighbors(x, botY);
+  incrementLivingNeighbors(rightX, botY);
+
+  return livingNeighbors;
+};
+
+const shouldDie = (x: number, y: number) => {
+  const numberOfLivingNeighbors = countLivingNeighbors(x, y);
+  return numberOfLivingNeighbors === 2 || numberOfLivingNeighbors === 3
+    ? false
+    : true;
+};
+
+const shouldRevive = (x: number, y: number) => {
+  const numberOfLivingNeighbors = countLivingNeighbors(x, y);
+  return numberOfLivingNeighbors === 3;
+};
+
+const generateDisplay = () => {
+  const display = [];
+  for (let i = 0; i < DISPLAY_HEIGHT; i++) {
+    const rowcolumns = [];
+    for (let j = 0; j < DISPLAY_WIDTH; j++) {
+      rowcolumns.push(
+        createElement("div", {
+          className: "life-container",
+          "data-x": j.toString(),
+          "data-y": i.toString(),
+          "data-living": Math.floor(Math.random() * 2) === 1,
+          onClick: (ev) => {
+            const x = Number(ev.currentTarget.dataset.x);
+            const y = Number(ev.currentTarget.dataset.y);
+            console.log(`x: ${typeof x}, y: ${y}`);
+            if (x && y) console.log(isAlive(x, y));
+          },
+        })
+      );
+    }
+    const row = createElement(
+      "div",
+      {
+        className: "life-row",
+      },
+      rowcolumns
+    );
+    display.push(row);
+  }
+  return display;
+};
+
+const stepLogic = (x: number, y: number) => {
+  if (isAlive(x, y)) return !shouldDie(x, y);
+  else return shouldRevive(x, y);
+};
+
+const stepDisplay = () => {
+  const newDisplay = [];
+  for (let i = 0; i < DISPLAY_HEIGHT; i++) {
+    const rowcolumns = [];
+    for (let j = 0; j < DISPLAY_WIDTH; j++) {
+      rowcolumns.push(
+        createElement("div", {
+          className: "life-container",
+          "data-x": j.toString(),
+          "data-y": i.toString(),
+          "data-living": `${stepLogic(j, i)}`,
+          onClick: (ev) => {
+            const x = Number(ev.currentTarget.dataset.x);
+            const y = Number(ev.currentTarget.dataset.y);
+            console.log(`x: ${typeof x}, y: ${y}`);
+            if (x && y) console.log(isAlive(x, y));
+          },
+        })
+      );
+    }
+    const row = createElement(
+      "div",
+      {
+        className: "life-row",
+      },
+      rowcolumns
+    );
+    newDisplay.push(row);
+  }
+  return newDisplay;
+};
+
+const Display = memo(({ displayRef }: Display) => {
+  const { run } = useGameContext();
+  let display = useMemo(() => generateDisplay(), []);
+  console.log(run);
+
+  useEffect(() => {
+    console.log("game is running");
+    display = stepDisplay();
+  }, [run]);
 
   return (
     <section className="display-container" ref={displayRef}>
-      {generateDisplay()}
+      {display}
     </section>
   );
 });
@@ -94,7 +206,11 @@ export default function App() {
   const displayRef = useRef<null | HTMLElement>(null);
   const [displayKey, displayKeyDispatch] = useReducer((x) => x + 1, 1);
 
+  const contextValue = useMemo(() => ({ run }), [run]);
+
   useEffect(() => {
+    console.log(displayRef.current);
+
     if (run) {
       intervalId.current = setInterval(() => {}, 400);
     }
@@ -102,16 +218,18 @@ export default function App() {
     return () => {
       clearInterval(intervalId.current);
     };
-  });
+  }, []);
 
   return (
     <>
-      <Display displayRef={displayRef} key={displayKey} />
-      <Controlls
-        run={run}
-        setRun={setRun}
-        displayKeyDispatch={displayKeyDispatch}
-      />
+      <GameContext.Provider value={contextValue}>
+        <Display displayRef={displayRef} key={displayKey} run={run} />
+        <Controlls
+          run={run}
+          setRun={setRun}
+          displayKeyDispatch={displayKeyDispatch}
+        />
+      </GameContext.Provider>
     </>
   );
 }
